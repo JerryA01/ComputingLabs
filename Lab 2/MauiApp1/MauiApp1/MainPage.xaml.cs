e69d949a4d9d3f2907e1af52b448ab398b8a5cd2
JerryA01 <jerryarthur075@gmail.com>
2026-02-03 14:33:33 +0000

    extended name

--- Lab 2/MauiApp1/MauiApp1/MainPage.xaml.cs
+++ Lab 2/MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -16,7 +16,7 @@
             if (count == 1)
                 CounterBtn.Text = $"Clicked {count} time";
             else
-                CounterBtn.Text = $"Jerry Clicked {count} times";
+                CounterBtn.Text = $"Jerry Arthur Clicked {count} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
         }
